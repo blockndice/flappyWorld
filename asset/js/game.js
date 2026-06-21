69794a6ae5cf2f70397a4a3ef5556fa869fe703f
blockndice <277510522+blockndice@users.noreply.github.com>
2026-06-21 09:44:22 +0200

    v0.16.2

--- asset/js/game.js
+++ asset/js/game.js
@@ -1081,7 +1081,7 @@ function drawUI() {
     if (intro1Page !== 4) {
       ctx.fillStyle = '#ffffff';
       ctx.font = '11px monospace';
-      ctx.fillText('v0.16.1', W/2, H - 14);
+      ctx.fillText('v0.16.2', W/2, H - 14);
     }
   }
 
